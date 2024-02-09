3edd51ca1ef54ee623f5cc3c992bd708e7228654
Nag, Ritvik <nagrim7@gmail.com>
2024-02-09 17:19:17 -0500

    add minified

---
+++ minify.sh
@@ -0,0 +1,16 @@
+
+URL='https://www.toptal.com/developers/javascript-minifier/api/raw'
+
+mkdir -p minified
+
+FILE=${1:-AddChatMarkersToSeq.jsx}
+OUT_FILE="minified/${FILE}"
+
+echo "[ Minifying ${FILE} ]"
+
+curl -X POST -s \
+	-H "Content-Type: application/x-www-form-urlencoded" \
+	--data-urlencode input@AddChatMarkersToSeq.jsx \
+	"${URL}" > "${OUT_FILE}"
+
+echo "Saved to \`${OUT_FILE}\`"
